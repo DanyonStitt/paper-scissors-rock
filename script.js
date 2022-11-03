a44e8c437dc23c29b9b34268093f6e34fa2834d7
Danyon <danyonstitt@gmail.com>
2022-11-03 19:04:02 +1300

    Added conditions for userchoice: paper and scissors

--- script.js
+++ script.js
@@ -8,24 +8,40 @@ function getComputerChoice() {
 
     // Select the choice at that index
     return compChoices[randIndex];
-}
+};
 
 // Compare the players choice to the computers choice
 function playRound(userChoice, compChoice) {
     // If both player and user make the same guess
-    if (userChoice.lower() === compChoice) {
+    if (userChoice.toLowerCase() === compChoice) {
         console.log("It's a draw! You both chose " + compChoice);
     }
     // If the user chooses rock
-    else if(text.lower(userChoice) === "rock") {
+    else if(userChoice.toLowerCase() === "rock") {
         // And the computer chose paper
         if(compChoice === "paper") {
             console.log("You lose! Paper beats rock");
         }
         // Or if the computer chose scissors
         else console.log("You won! Rock beats " + compChoice);
+    }
+    
+    // If the user chooses paper
+    else if(userChoice.toLowerCase() === "paper") {
+        if (compChoice === "rock") {
+            console.log("You won! Paper beats rock");
+        }
+        else console.log("You lose! scissors beat paper");
+    }
+
+    // If the user chooses scissors
+    else if(userChoice.toLowerCase === "scissors") {
+        if(compChoice === "rock") {
+            console.log("You lose! Rock beats scissors");
+        }
+        else console.log("You won! Scissors beat paper");
     };
-}
+};
 
 // Get the computers choice
 const compChoice = getComputerChoice();
