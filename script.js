2249a0211f8b72491e7ceb6d47f37320b11b6668
Danyon <danyonstitt@gmail.com>
2022-11-05 09:18:27 +1300

    Added 5 rounds with final winner

--- script.js
+++ script.js
@@ -14,50 +14,63 @@ function getComputerChoice() {
 function playRound(userChoice, compChoice) {
     // If both player and user make the same guess
     if (userChoice.toLowerCase() === compChoice) {
-        console.log("It's a draw! You both chose " + compChoice);
+        return "It's a draw! You both chose " + compChoice;
     }
     // If the user chooses rock
     else if(userChoice.toLowerCase() === "rock") {
-        // And the computer chose paper
-        if(compChoice === "paper") {
-            console.log("You lose! Paper beats rock");
-        }
-        // Or if the computer chose scissors
-        else console.log("You won! Rock beats " + compChoice);
+        if(compChoice === "paper") {return "You lose! Paper beats rock";}
+        else return "You won! Rock beats " + compChoice;
     }
-    
     // If the user chooses paper
     else if(userChoice.toLowerCase() === "paper") {
-        if (compChoice === "rock") {
-            console.log("You won! Paper beats rock");
-        }
-        else console.log("You lose! scissors beat paper");
+        if (compChoice === "rock") {"You won! Paper beats rock";}
+        else return "You lose! scissors beat paper";
     }
-
     // If the user chooses scissors
-    else if(userChoice.toLowerCase === "scissors") {
-        if(compChoice === "rock") {
-            console.log("You lose! Rock beats scissors");
-        }
-        else console.log("You won! Scissors beat paper");
+    else if(userChoice.toLowerCase() === "scissors") {
+        if(compChoice === "rock") {return "You lose! Rock beats scissors";}
+        else return "You won! Scissors beat paper";
     }
-    else alert("Wrong! Choose either paper, scissors, or rock!")
+    // if the input is not rock, paper, or scissors, tell the user to pick again
+    else return "Wrong! Choose either paper, scissors, or rock!";
 };
 
-
-
-
-for (let i = 0; i < 5; i++) {
-    // Get the computers choice
-    const compChoice = getComputerChoice();
-
-    // ask the user for an input 
-    const userChoice = prompt("Paper, scissors, or rock?");
-    
-    playRound(userChoice, compChoice);
+function game() {
+    // Set a counter for the number of wins and loses
+    let wins = 0;
+    let loses = 0;
+    alert("Playing best of 5 games");
+    // Play 5 rounds of the game
+    for (let i = 0; i < 5; i++) {
+        // Get the computers choice
+        const compChoice = getComputerChoice();
+        // ask the user for an input 
+        const userChoice = prompt("Paper, scissors, or rock?");
+        // Play the game
+        let gameResult = playRound(userChoice, compChoice);
+        console.log(gameResult);
+        if (gameResult.toLowerCase().includes("you win!")) {
+            wins = wins + 1;
+        }
+        else if (gameResult.toLowerCase().includes("you lose!")) {
+            loses = loses + 1
+        };
+        console.log(gameResult)
+    };
+    // showing the final winner
+    if (wins > loses) {
+        console.log("Well done! you won " + wins + " to " + loses);
+    }
+    else if (loses > wins) {
+        console.log("Haha! I win " + loses + " to " + wins);
+    }
+    else console.log("Play me again!");
 }
 
 
-// if the input is not rock, paper, or scissors
-// tell the user to pick again
+game()
+
+
+
+// 
 // 
